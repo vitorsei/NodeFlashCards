7791669d01e062a5624e6daa0fc2a4028f28f6fe
vitorsei <seiji.vitor@gmail.com>
2016-12-16 21:02:11 -0200

    Refactoring functions

--- app.js
+++ app.js
@@ -11,28 +11,6 @@ var vocabService = require('./src/vocabService')();
 var vocabEntryPath = "D:\\Seiji\\Vocab\\EngVocab.txt";
 var vocabOutputPath = "D:\\Seiji\\Vocab\\Test.txt";
 
-var vocabs = [];
-
 var lines = ReadFile(vocabEntryPath);
-console.log(lines);
-
-
-var word = '';
-var explanations = [];
-
-var length = lines.length;
-for (var i = 0; i < length; i++) {
-    if (!vocabService.IsNullOrWhiteSpace(lines[i])) {
-        if (vocabService.IsNewWord(lines[i])) {
-            word = vocabService.Sanitize(lines[i]);
-            explanations = vocabService.ExtractExplanations(lines, ++i);
-            vocabs.push(new Vocab(word, explanations));
-        }
-    }
-}
-
-var formattedVocabs = vocabs.map(function (vocab) {
-    return vocab.word + '\t' + vocab.explanations.join('||');
-});
-
-WriteFile(vocabOutputPath, formattedVocabs);
+var vocabs = vocabService.ExtractVocab(lines);
+WriteFile(vocabOutputPath, vocabs);
--- src/vocabService.js
+++ src/vocabService.js
@@ -1,11 +1,13 @@
+var Vocab = require('./vocab');
+
 var vocabService = function () {
-    var Sanitize = function (string) {
+    function Sanitize(string) {
         return string
             .replace('\r', '')
             .replace('\t', '')
             .replace('\n', '')
             .trim();
-    };
+    }
 
     var IsNullOrWhiteSpace = function (string) {
         return !/\S/.test(string);
@@ -28,14 +30,37 @@ var vocabService = function () {
     var IsNewWord = function (word) {
         var regex = new RegExp('^[a-z|\s]', 'gi');
         return word.match(regex);
+    };
+
+    function FormatVocab(vocabs) {
+        var formattedVocabs = vocabs.map(function (vocab) {
+            return vocab.word + '\t' + vocab.explanations.join('||');
+        });
+
+        return formattedVocabs;
+    }
+
+    function ExtractVocab(lines) {
+        var word = '';
+        var explanations = [];
+        var vocabs = [];
+
+        var length = lines.length;
+        for (var i = 0; i < length; i++) {
+            if (!IsNullOrWhiteSpace(lines[i])) {
+                if (IsNewWord(lines[i])) {
+                    word = Sanitize(lines[i]);
+                    explanations = ExtractExplanations(lines, ++i);
+                    vocabs.push(new Vocab(word, explanations));
+                }
+            }
+        }
+
+        return FormatVocab(vocabs);
     }
 
     return {
-        Sanitize: Sanitize,
-        IsNullOrWhiteSpace: IsNullOrWhiteSpace,
-        IsNewExplanation: IsNewExplanation,
-        ExtractExplanations: ExtractExplanations,
-        IsNewWord: IsNewWord
+        ExtractVocab: ExtractVocab
     };
 };
 
