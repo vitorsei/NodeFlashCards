acc8e51fdff192ba5868a6d72d9945ba80ea9a59
vitorsei <seiji.vitor@gmail.com>
2016-12-16 21:06:51 -0200

    Renaming variables

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ var ReadFile = require('./src/fileService').readFile;
 var WriteFile = require('./src/fileService').writeFile;
 var vocabService = require('./src/vocabService')();
 
-var vocabEntryPath = "D:\\Seiji\\Vocab\\EngVocab.txt";
-var vocabOutputPath = "D:\\Seiji\\Vocab\\Test.txt";
+var entryPah = "D:\\Seiji\\Vocab\\EngVocab.txt";
+var outputPath = "D:\\Seiji\\Vocab\\Test.txt";
 
-var lines = ReadFile(vocabEntryPath);
+var lines = ReadFile(entryPah);
 var vocabs = vocabService.ExtractVocab(lines);
-WriteFile(vocabOutputPath, vocabs);
+WriteFile(outputPath, vocabs);
